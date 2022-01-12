04b82b76578a541a14477307fdb98676ad7151a3
vladkurtash <82370921+vladkurtash@users.noreply.github.com>
2022-01-12 19:47:02 +0200

    BugFix: ObjectDestroyer
    Now deletes correctly

--- Assets/_Project/Scripts/Sandbox/ObjectDestroyer.cs
+++ Assets/_Project/Scripts/Sandbox/ObjectDestroyer.cs
@@ -4,13 +4,15 @@ namespace ChopChop
 {
     public class ObjectDestroyer : MonoBehaviour
     {
-        private float _distanceToDestroy = 3.0f;
+        private float _distanceToDestroy = 5.0f;
         private Transform _cameraTransform = null;
         private Vector3 _vectorToCamera = Vector3.zero;
+        private Transform _transform = null;
 
         private void Awake()
         {
             _cameraTransform = Camera.main.transform;
+            _transform = this.gameObject.GetComponent<Transform>();
         }
 
         private void LateUpdate()
@@ -22,23 +24,28 @@ namespace ChopChop
         {
             _vectorToCamera = (_cameraTransform.position - this.gameObject.transform.position);
 
-            if (!BehindCamera(_vectorToCamera, Camera.main.transform.forward))
-                return;
-
-            if (ObjectFarEnoughBehindCamera())
+            if (BehindCamera(_vectorToCamera, _transform.position))
             {
-                Destroy(this.gameObject);
+                if (ObjectFarEnoughBehindCamera())
+                    Destroy(this.gameObject);
             }
 
             #region LocalFunctions
 
             bool BehindCamera(Vector3 vector1, Vector3 vector2) =>
-                Vector3.Angle(_vectorToCamera, Camera.main.transform.forward) > 90;
+                Vector3.Dot(vector1, vector2) < 0;
 
             bool ObjectFarEnoughBehindCamera()
                 => _vectorToCamera.sqrMagnitude > _distanceToDestroy * _distanceToDestroy;
 
             #endregion
+
+#if UNITY_EDITOR
+            if (!BehindCamera(_vectorToCamera, _transform.position))
+                Debug.DrawLine(_cameraTransform.position, _transform.position, Color.green);
+            else if (BehindCamera(_vectorToCamera, _transform.position))
+                Debug.DrawLine(_cameraTransform.position, _transform.position, Color.red);
+#endif
         }
     }
 }
