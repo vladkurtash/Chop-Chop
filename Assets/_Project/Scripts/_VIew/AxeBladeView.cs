38bbc37820522147363fd435714d9a451ffcadf8
vladkurtash <82370921+vladkurtash@users.noreply.github.com>
2022-01-12 16:21:34 +0200

    Axe ScoreMultiplier RayDetector changes

--- Assets/_Project/Scripts/_VIew/AxeBladeView.cs
+++ Assets/_Project/Scripts/_VIew/AxeBladeView.cs
@@ -8,6 +8,8 @@ namespace ChopChop
         public event Action<Collider> OnTriggerEnterEvent;
         public event Action<RaycastHit> OnRaycastHitEvent;
 
+        [SerializeField] private float rayLength;
+
         private void OnTriggerEnter(Collider otherCollider)
         {
             OnTriggerEnterEvent?.Invoke(otherCollider);
@@ -15,15 +17,19 @@ namespace ChopChop
 
         private void FixedUpdate()
         {
-            if(Physics.Raycast(transform.position, -transform.right, out RaycastHit hit, 0.1f))
+            if (Physics.Raycast(transform.position, -transform.right, out RaycastHit hit, rayLength))
             {
+#if UNITY_EDITOR
                 Debug.DrawRay(transform.position, -transform.right * hit.distance, Color.green);
+#endif
                 OnRaycastHitEvent?.Invoke(hit);
             }
+#if UNITY_EDITOR
             else
             {
-                Debug.DrawRay(transform.position, -transform.right * 0.1f, Color.red);
+                Debug.DrawRay(transform.position, -transform.right * rayLength, Color.red);
             }
+#endif
         }
 
 #if UNITY_EDITOR
@@ -32,6 +38,8 @@ namespace ChopChop
             EzySlice.Plane cuttingPlane = new EzySlice.Plane();
             cuttingPlane.Compute(transform);
             cuttingPlane.OnDebugDraw();
+
+            Debug.DrawRay(transform.position, -transform.right * rayLength, Color.red);
         }
 #endif
     }
